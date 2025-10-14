c19d3f3421b6d2a7c1687910fd7e982ded0b81f7
souzafcharles <souzach@outlook.com>
2025-10-14 18:03:14 -0300

    chore: initial asteroid alerting service project setup

---
+++ asteroid-alerting-service/src/main/java/com/github/souzafcharles/asteroid_alerting_service/AsteroidAlertingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.souzafcharles.asteroid_alerting_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteroidAlertingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteroidAlertingServiceApplication.class, args);
+	}
+
+}
---
+++ asteroid-alerting-service/src/test/java/com/github/souzafcharles/asteroid_alerting_service/AsteroidAlertingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.souzafcharles.asteroid_alerting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteroidAlertingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
